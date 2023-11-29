d52a7188dd96b5bac78d1620ce9df994026f48d4
Egor Chubkov <ea.chubkov@gmail.com>
2023-11-29 20:10:00 +0300

    hw 9

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,41 +2,47 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import pages.RegistrationPage;
 import pages.TextBoxPage;
+
+import java.util.Locale;
 
 public class TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
     TextBoxPage textBoxPage = new TextBoxPage();
 
+    Faker faker = new Faker();
+
     final String
             urlRegistrationPage = "/automation-practice-form",
-            firstName = "Капибара",
-            lastName = "0_0",
-            email = "capibara@gmail.com",
+            firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
             gender = "Male",
-            number = "1234567890",
+            number = faker.phoneNumber().subscriberNumber(10),
             year_of_birth = "2000",
             month_of_birth = "November",
             day_of_birth = "25",
             subject = "Arts",
             hobby = "Sports",
             fileUrl = "capybara.jpg",
-            address = "Пруд с апельсинами",
+            address = faker.address().cityName(),
             state = "NCR",
             city = "Delhi",
             urlTextBoxPage = "/text-box",
-            currentAddress = "Mars 2",
-            permanentAddress = "Mars";
+            currentAddress = faker.address().streetAddress(),
+            permanentAddress = faker.address().secondaryAddress();
 
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
     }
 
     @AfterEach
