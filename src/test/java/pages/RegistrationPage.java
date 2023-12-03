dd18db80c5791af1d5f6dc3bd18a3beddee0f434
Egor Chubkov <ea.chubkov@gmail.com>
2023-12-03 19:50:24 +0300

    hw 13 some fix

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -2,10 +2,12 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
+import org.junit.jupiter.api.Tag;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
+@Tag("registration")
 public class RegistrationPage extends PageBase {
 
     SelenideElement firstNameInput = $("#firstName"),
