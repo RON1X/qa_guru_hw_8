a7a5a024a687ac7294d7eff14da941f465758ea2
Egor Chubkov <ea.chubkov@gmail.com>
2023-12-03 19:33:24 +0300

    hw 13

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -19,77 +21,92 @@ public class RegistrationPage extends PageBase {
             cityInput = $("#react-select-4-input"),
             submitButton = $("#submit");
 
+    @Step("Открыть страницу {url}")
     public RegistrationPage openPage(String url) {
         open(url);
         removeBanner();
         return this;
     }
 
+    @Step("Указать имя {firstName}")
     public RegistrationPage setFirstName(String firstName) {
         firstNameInput.setValue(firstName);
         return this;
     }
 
+    @Step("Указать фамилию {lastName}")
     public RegistrationPage setLastName(String lastName) {
         lastNameInput.setValue(lastName);
         return this;
     }
 
+    @Step("Указать email {email}")
     public RegistrationPage setEmail(String email) {
         emailInput.setValue(email);
         return this;
     }
 
+    @Step("Выбрать пол {gender}")
     public RegistrationPage setGender(String gender) {
         genderSelect.$(byText(gender)).click();
         return this;
     }
 
+    @Step("Указать номер телефона {number}")
     public RegistrationPage setNumber(String number) {
         numberInput.setValue(number);
         return this;
     }
 
+    @Step("Указать дату рождения {day}.{month}.{year}")
     public RegistrationPage setDateOfBirth(String day, String month, String year) {
         calendarComponent.setDate(day, month, year);
         return this;
     }
 
+    @Step("Указать предмет {subject}")
     public RegistrationPage setSubject(String subject) {
         subjectsInput.setValue(subject).pressEnter();
         return this;
     }
 
+    @Step("Указать хобби {hobby}")
     public RegistrationPage setHobby(String hobby) {
         hobbiesSelect.$(byText(hobby)).click();
         return this;
     }
 
+    @Step("Загрузить картинку {fileUrl}")
     public RegistrationPage uploadImage(String fileUrl) {
         uploadPictureInput.uploadFromClasspath(fileUrl);
         return this;
     }
 
+    @Step("Указать адрес {address}")
     public RegistrationPage setAddress(String address) {
         addressInput.setValue(address);
         return this;
     }
 
+    @Step("Указать штат {state}")
     public RegistrationPage setState(String state) {
         stateInput.setValue(state).pressEnter();
         return this;
     }
 
+    @Step("Указать город {city}")
     public RegistrationPage setCity(String city) {
         cityInput.setValue(city).pressEnter();
         return this;
     }
 
+    @Step("Нажать кнопку Submit")
     public RegistrationPage clickSubmitButton() {
         submitButton.click();
         return this;
     }
 
+    @Step("Проверить что в поле {key} находится значение {value}")
     public RegistrationPage verifyResult(String key, String value) {
         tableComponent.verifyResult(key, value);
         return this;
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,13 +2,16 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 import pages.TextBoxPage;
-
-import java.util.Locale;
+import java.util.Map;
 
 public class TestBase {
 
@@ -43,10 +46,22 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = false;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
     void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         Selenide.closeWebDriver();
     }
 }
